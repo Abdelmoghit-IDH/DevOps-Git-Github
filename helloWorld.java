0360dc17046764ae62108cca71ac68ac53706970
moulinehatim <hatimmouline2014@gmail.com>
2022-02-25 11:41:31 +0100

    test jenkins 4

--- helloWorld.java
+++ helloWorld.java
@@ -3,6 +3,6 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World built with jenkins 3");
+		System.out.println("Hello World built with jenkins 4");
 	}
 }
